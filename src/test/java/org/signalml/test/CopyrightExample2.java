3f245a3619581e0ffced8a3a9763719e34f78094
Michal Dobaczewski <michal.d08@gmail.com>
2009-02-26 08:55:04 +0100

    zmiany w site, testowe pliki do (C)

---
+++ src/test/java/org/signalml/test/CopyrightExample2.java
@@ -0,0 +1,52 @@
+/* Svarog - the open source signal viewer, analyzer and recorder.               
+ * Copyright (C) 2007 Zakład Fizyki Biomedycznej, Universitas Varsoviensis
+ *
+ * This file is part of Svarog.                                                 
+ *                                                                              
+ * Svarog is free software: you can redistribute it and/or modify               
+ * it under the terms of the GNU General Public License as published by         
+ * the Free Software Foundation, either version 3 of the License, or            
+ * (at your option) any later version.                                          
+ *                                                                              
+ * Svarog is distributed in the hope that it will be useful,                    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
+ * GNU General Public License for more details.                                 
+ *                                                                              
+ * You should have received a copy of the GNU General Public License            
+ * along with Svarog.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 
+ * Credits: 
+ * 	
+ *      CC Otwarte Systemy Komputerowe Sp. z o.o. <http://www.cc.com.pl/>
+ *  
+ */                                                                             
+
+package org.signalml.test;
+
+
+/** 
+ * This class is pointless, but it's documented.
+ * 
+ * @author Michał Dobaczewski &lt;michal (dot) d08 (at) gmail (dot-com)&gt;
+ * @author Oskar Kapala
+ * @author Hieronim Iksiński
+ *  
+ * @org.signalml.thanks CC Otwarte Systemy Komputerowe Sp. z o.o.
+ * @org.signalml.copyright Copyright &copy; 2007 Zakład Fizyki Biomedycznej, Universitas Varsoviensis (GPLv3 license - see source file)
+ *
+ */
+public class CopyrightExample2 {
+
+	/**
+	 * Test.
+	 * 
+	 * @return test
+	 */
+	public String makeString()
+	{
+		return "";
+	}
+
+}
