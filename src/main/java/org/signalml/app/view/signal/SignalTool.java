47181506af49da91e2a42a8577d3950e557e05ea
Stanislaw Findeisen <stf@eisenbits.com>
2010-10-06 16:52:55 +0200

    Slight Javadoc fix.

--- src/main/java/org/signalml/app/view/signal/SignalTool.java
+++ src/main/java/org/signalml/app/view/signal/SignalTool.java
@@ -12,17 +12,19 @@ import javax.swing.event.MouseInputListener;
 public interface SignalTool extends MouseInputListener, MouseWheelListener {
 
     /**
+     * Tells if this SignalTool is processing a mouse event sequence.
+     * <p>
      * Mouse actions are often composed of multiple events, for instance:
      * {@link MouseInputListener#mousePressed(java.awt.event.MouseEvent)},
      * {@link MouseInputListener#mouseDragged(java.awt.event.MouseEvent)},
      * {@link MouseInputListener#mouseReleased(java.awt.event.MouseEvent)}.
      * Recognized sequences and their semantics are implementation dependent.
-     * 
+     * <p>
      * The value returned by this method serves as an indicator that this SignalTool
      * is currently involved in processing some mouse action and that other mouse
      * triggered actions associated with the same {@link SignalView} instance (like
      * popup menu) should be blocked. This helps to avoid interference.
-     * 
+     * <p>
      * A sample implementation can store a boolean variable and set it to true in
      * {@link MouseInputListener#mousePressed(java.awt.event.MouseEvent)} and to false
      * in {@link MouseInputListener#mouseReleased(java.awt.event.MouseEvent)}.
