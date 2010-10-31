bcf267fc64892eb24296675d91a8d72bf15b3f73
Zbyszek Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2010-10-31 12:52:11 +0100

    Add .dir-locals.el to set emacs buffer variables

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 8)
+         (fill-column . 80)))
+ (java-mode . ((c-basic-offset . 8)))
+ (sgml-mode . ((sgml-basic-offset . 8)))
+ ; emacs complains about sgml-basic-offset being unsafe, so let's set it only for XML
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)))
+)
