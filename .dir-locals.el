16a8a1b5bfb945714c07d2993d88a46a5b54bae0
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2010-11-11 10:37:14 +0100

    Clean up developers list & add the tags team

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
          (fill-column . 80)))
  (java-mode . ((c-basic-offset . 8)))
  (sgml-mode . ((sgml-basic-offset . 8)))
+ (nxml-mode . ((nxml-child-indent . 8)))
  ; emacs complains about sgml-basic-offset being unsafe, so let's set it only for XML
  (emacs-lisp-mode . ((indent-tabs-mode . nil)))
 )
