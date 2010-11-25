277b3c30e18e0f3969d3136feb0c5d41f3d31cdd
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2010-11-25 11:45:42 +0100

    Change displayed Svarog version to 0.5.0-SNAPSHOT

--- src/main/java/org/signalml/util/SvarogConstants.java
+++ src/main/java/org/signalml/util/SvarogConstants.java
@@ -21,7 +21,7 @@ public abstract class SvarogConstants {
 	/**
 	 * Actual version of the project
 	 */
-	public static final String VERSION = "0.4.7";
+	public static final String VERSION = "0.5.0-SNAPSHOT";
 
 	/**
 	 * Recommended minimal dimension of the screen
