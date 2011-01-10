de7139417d9824c149729af4de77a7d220cd4805
Marcin Szumski <ms248260@students.mimuw.edu.pl>
2011-01-10 22:04:55 +0100

    plug-in with the signal tool (SignalFFTTool) which displays the power spectrum

---
+++ src/plugins/FFTSignalTool/src/org/signalml/plugin/fftsignaltool/SignalFFTTool.java
@@ -0,0 +1,298 @@
+package org.signalml.plugin.fftsignaltool;
+
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.io.InvalidClassException;
+
+import javax.swing.JLayeredPane;
+import javax.swing.SwingUtilities;
+
+import org.signalml.app.util.IconUtils;
+import org.signalml.plugin.export.SvarogAccess;
+import org.signalml.plugin.export.signal.AbstractSignalTool;
+import org.signalml.plugin.export.signal.SignalTool;
+import org.signalml.plugin.export.view.ExportedSignalPlot;
+import org.signalml.plugin.export.view.ExportedSignalView;
+import org.signalml.plugin.fft.export.FourierTransform;
+import org.springframework.context.support.MessageSourceAccessor;
+
+/**
+ * {@link SignalTool Signal tool} which displays the {@link SignalFFTPlot plot}
+ * with the {@link FourierTransform#powerSpectrumReal(double[], double) power
+ * spectrum} of the part of the signal near the cursor when the left button of
+ * the mouse is pressed.<p>
+ * The settings how the power spectrum should be displayed are stored in
+ * {@link SignalFFTSettings}.
+ * 
+ * @author Michal Dobaczewski &copy; 2007-2008 CC Otwarte Systemy Komputerowe
+ *         Sp. z o.o., Marcin Szumski
+ */
+public class SignalFFTTool extends AbstractSignalTool {
+
+	/**
+	 * the {@link ExportedSignalPlot signal plot} for which this FFT tool is
+	 * created
+	 */
+	private ExportedSignalPlot plot;
+	/**
+	 * the {@link SignalFFTPlot plot} on which the power spectrum is displayed
+	 */
+	private SignalFFTPlot fftPlot;
+	/**
+	 * the source of messages (labels)
+	 */
+	private MessageSourceAccessor messageSource;
+
+	/**
+	 * the {@link SvarogAccess access} to Svarog logic and GUI
+	 */
+	private SvarogAccess svarogAccess;
+
+	/**
+	 * {@link SignalFFTSettings settings} how the power spectrum should be
+	 * displayed
+	 */
+	private SignalFFTSettings settings;
+
+	/**
+	 * Constructor. Sets the source of messages and the {@link
+	 * ExportedSignalView signal view} for which this FFT tool is
+	 * created.
+	 * @param signalView the signal view
+	 * @param messageSource the source of messages
+	 */
+	public SignalFFTTool(ExportedSignalView signalView,
+			MessageSourceAccessor messageSource) {
+		super(signalView);
+		fftPlot = new SignalFFTPlot(messageSource);
+		// fftPlot = new SignalFFTPlot();
+		settings = new SignalFFTSettings();
+	}
+
+	/**
+	 * Constructor. Sets the source of messages. The {@link
+	 * ExportedSignalView signal view} for which this FFT tool is
+	 * created must be set separately.
+	 * @param messageSource the source of messages
+	 */
+	public SignalFFTTool(MessageSourceAccessor messageSource) {
+		super();
+		fftPlot = new SignalFFTPlot(messageSource);
+		settings = new SignalFFTSettings();
+		this.messageSource = messageSource;
+	}
+
+	/**
+	 * Sets the source of messages (labels).
+	 * @param messageSource the source of messages (labels)
+	 */
+	public void setMessageSource(MessageSourceAccessor messageSource) {
+		this.messageSource = messageSource;
+	}
+
+	@Override
+	public SignalFFTTool createCopy() {
+		SignalFFTTool copy = new SignalFFTTool(messageSource);
+		copy.setSvarogAccess(svarogAccess);
+		copy.setSettings(getSettings());
+		return copy;
+	}
+
+	@Override
+	public Cursor getDefaultCursor() {
+		return IconUtils.getCrosshairCursor();
+	}
+
+	/**
+	 * Displays the {@link SignalFFTPlot plot} with the power spectrum of the
+	 * part of the signal near the cursor.
+	 */
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+		if (SwingUtilities.isLeftMouseButton(e)) {
+
+			Object source = e.getSource();
+			if (!(source instanceof ExportedSignalPlot)) {
+				plot = null;
+				return;
+			}
+			plot = (ExportedSignalPlot) source;
+
+			Point point = e.getPoint();
+			int channel = plot.toChannelSpace(point);
+			fftPlot.setSettings(settings);
+			if (e.isControlDown()) {
+				Dimension plotSize = settings.getPlotSize();
+				int width = Math.min(800, plotSize.width * 2);
+				int height = Math.min(600, plotSize.height * 2);
+				Dimension size = new Dimension(width, height);
+				fftPlot.setPlotSize(size);
+				fftPlot.setWindowWidth(2 * settings.getWindowWidth());
+			}
+			fftPlot.setParameters(plot, point, channel);
+			showFFT(point);
+			selectAround(point);
+			setEngaged(true);
+			e.consume();
+
+		}
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if (SwingUtilities.isLeftMouseButton(e)) {
+			hideFFT();
+			setEngaged(false);
+			plot = null;
+			e.consume();
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+
+		if (plot != null) {
+			if (SwingUtilities.isLeftMouseButton(e)) {
+				Point point = e.getPoint();
+				Rectangle r = new Rectangle(point.x, point.y, 1, 1);
+				((ExportedSignalPlot) e.getSource()).scrollRectToVisible(r);
+				if (settings.isChannelSwitching()) {
+					int channel = plot.toChannelSpace(point);
+					fftPlot.setParameters(point, channel);
+				} else {
+					fftPlot.setFocusPoint(point);
+				}
+				positionFFT(point, null);
+				selectAround(point);
+			}
+		}
+
+	}
+
+	/**
+	 * Returns the {@link SignalFFTSettings settings} how the power spectrum
+	 * should be displayed.
+	 * @return the settings how the power spectrum should be displayed
+	 */
+	public SignalFFTSettings getSettings() {
+		return settings;
+	}
+
+	/**
+	 * Sets the {@link SignalFFTSettings settings} how the power spectrum should be
+	 * displayed.
+	 * @param settings the settings how the power spectrum should be displayed
+	 */
+	public void setSettings(SignalFFTSettings settings) {
+		if (settings == null) {
+			throw new NullPointerException("No settings");
+		}
+		this.settings = settings;
+	}
+
+	/**
+	 * Calculates the position on the screen where the {@link SignalFFTPlot
+	 * plot} with the power spectrum should be displayed and displays it at
+	 * that place.
+	 * @param point the location of the cursor
+	 * @param layeredPane the pane on which the fft plot should be displayed
+	 */
+	private void positionFFT(Point point, JLayeredPane layeredPane) {
+		if (plot != null) {
+			if (layeredPane == null) {
+				layeredPane = plot.getRootPane().getLayeredPane();
+			}
+			Dimension size = fftPlot.getPreferredSize();
+			int channel = fftPlot.getChannel();
+			int channelY = plot.channelToPixel(channel);
+			Point location = SwingUtilities.convertPoint((Component) plot,
+					new Point(point.x, channelY), layeredPane);
+			int y;
+			if (location.y > layeredPane.getHeight() / 2) {
+				y = location.y - size.height;
+			} else {
+				y = location.y + plot.getPixelPerChannel();
+			}
+			fftPlot.setBounds(location.x - (size.width / 2), y, size.width,
+					size.height);
+		}
+	}
+
+	/**
+	 * Displays the {@link SignalFFTPlot fft plot} with the power spectrum of
+	 * the part of the signal near cursor ({@code point})
+	 * @param point the location of the cursor
+	 */
+	private void showFFT(Point point) {
+		if (plot != null) {
+			fftPlot.setVisible(true);
+			JLayeredPane layeredPane = plot.getRootPane().getLayeredPane();
+			positionFFT(point, layeredPane);
+			layeredPane.add(fftPlot, new Integer(JLayeredPane.DRAG_LAYER));
+		}
+	}
+
+	/**
+	 * Hides the {@link SignalFFTPlot fft plot} (removes it from
+	 * {@link ExportedSignalPlot signal plot}).
+	 */
+	private void hideFFT() {
+		if (plot != null) {
+			JLayeredPane layeredPane = plot.getRootPane().getLayeredPane();
+			layeredPane.remove(fftPlot);
+			plot.getRootPane().repaint();
+		}
+	}
+
+	/**
+	 * Draws the rectangle around point on the currently selected channel which
+	 * is nearest to the specified {@code point}.
+	 * If the point is not located on this channel, 
+	 * @param point the point
+	 */
+	private void selectAround(Point point) {
+		if (plot != null) {
+			Float centerPosition = plot.toTimeSpace(point);
+			if (centerPosition != null) {
+				double offset = (((float) settings.getWindowWidth()) / plot
+						.getSamplingFrequency()) / 2;
+				Float startPosition = new Float(centerPosition.floatValue()
+						- ((float) offset));
+				Float endPosition = new Float(centerPosition.floatValue()
+						+ ((float) offset));
+				if (startPosition.equals(endPosition)) {
+					getSignalView().clearSignalSelection();
+				} else {
+					Integer channel = fftPlot.getChannel();
+					if (channel != null) {
+						try {
+							getSignalView().setSignalSelection(
+									plot,
+									plot.getChannelSelection(startPosition,
+											endPosition, channel));
+						} catch (InvalidClassException e) {
+							throw new RuntimeException("invalid plot");
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Sets the {@link SvarogAccess access} to elements of Svarog in this class
+	 * and in the {@link SignalFFTPlot plot}.
+	 * @param access the access to elements of Svarog
+	 */
+	public void setSvarogAccess(SvarogAccess access) {
+		svarogAccess = access;
+		fftPlot.setSvarogAccess(access);
+	}
+
+}
