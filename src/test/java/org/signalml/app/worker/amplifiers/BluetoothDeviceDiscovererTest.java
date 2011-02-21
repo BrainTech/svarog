e565b3d34d3b34bb3de512a4e0a1b610db5667fd
Tomasz Sawicki <tomasz.sawicki.89@gmail.com>
2011-02-21 14:51:48 +0100

    Added device discoverers' tests.

---
+++ src/test/java/org/signalml/app/worker/amplifiers/BluetoothDeviceDiscovererTest.java
@@ -0,0 +1,56 @@
+package org.signalml.app.worker.amplifiers;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests {@link BluetoothDeviceDiscoverer}.
+ *
+ * @author Tomasz Sawicki
+ */
+public class BluetoothDeviceDiscovererTest implements PropertyChangeListener {
+
+        private BluetoothDeviceDiscoverer discoverer;
+        private final Object event = new Object();
+
+        @Test
+        public void test() {
+
+                discoverer = new BluetoothDeviceDiscoverer();
+                discoverer.addPropertyChangeListener(this);
+
+                try {
+                        System.out.println("Searching...");
+                        discoverer.startSearch();
+                } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                }
+                
+                synchronized(event) {
+                        try {
+                                event.wait();
+                        } catch (InterruptedException ex) {
+                        }
+                }
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+
+                if ("deviceFound".equals(evt.getPropertyName())) {
+
+                        DeviceInfo info = (DeviceInfo) evt.getNewValue();
+                        System.out.println("Device found: " + info.getName() + " " + info.getAddress() + " (" + info.getDeviceType() + ")");
+                }
+                else if ("endOfSearch".equals(evt.getPropertyName())) {
+
+                        System.out.println("End of search!");
+                        
+                        synchronized(event) {
+                                event.notify();
+                        }
+                }
+        }
+}
