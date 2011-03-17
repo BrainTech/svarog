f5ead3f86abedb2a0017658115a2dac8d7416f67
Marcin Szumski <ms248260@students.mimuw.edu.pl>
2011-03-17 14:55:13 +0100

    Installer for Svarog
    'mvn install' in src/izpack directory creates two installers:
    - jar installer for windows/linux/mac
    - exe installer for windows

---
+++ src/izpack/mac/svarog.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java -Xmx256m -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -jar svarog.jar 2>&1 > /dev/null & 
