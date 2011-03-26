cb93b138cee83483890383d10d5a3ffe3cb9addb
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2011-03-26 13:22:27 +0100

    Fixed previous bug fix...

--- src/main/java/org/signalml/app/view/opensignal/OpenSignalAndSetMontageDialogManager.java
+++ src/main/java/org/signalml/app/view/opensignal/OpenSignalAndSetMontageDialogManager.java
@@ -67,7 +67,6 @@ public class OpenSignalAndSetMontageDialogManager implements PropertyChangeListe
 			int montageSourceChannelCount = currentMontage.getSourceChannelCount();
 			int signalSourcePanelChannelCount = signalSourcePanel.getCurrentSignalSourcePanel().getChannelCount();
 
-			if (montageSourceChannelCount != signalSourcePanelChannelCount)
 			numberOfChannelsChangedTo(signalSourcePanelChannelCount);
 
 			samplingFrequencyChangedTo(signalSourcePanel.getCurrentSignalSourcePanel().getSamplingFrequency());
