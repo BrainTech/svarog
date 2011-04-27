1dd0d9c5382c9fa5171dffb0899cdd9ec5e0610d
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-04-27 12:40:01 +0200

    Centralize dependency management
    Putting svarog as a 'provided' dependency ensures that all of svarog
    dependencies are available to plugins, both during compilation and at runtime.

--- util/build.sh
+++ util/build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 rm -rf ~/.m2/repository/org/signalml/svarog
+( cd util/maven && mvn -N install )
 mvn -N install
 for p in src/plugins/{FFT,FFTSignalTool,PluginToolCommon,Artifact} src/izpack; do
     (
