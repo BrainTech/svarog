ccd97bf1682a708ce4528bad849997b2afd5f168
Stanislaw Findeisen <stf@eisenbits.com>
2011-10-21 21:38:12 +0200

    New SFTestPlugin features that demonstrate the new i18n services provided by plugin API.

--- plugins/SFTestPlugin/src/main/java/org/signalml/plugin/sf/SFTestPlugin.java
+++ plugins/SFTestPlugin/src/main/java/org/signalml/plugin/sf/SFTestPlugin.java
@@ -1,5 +1,7 @@
 package org.signalml.plugin.sf;
 
+import java.text.MessageFormat;
+
 import org.signalml.app.logging.SvarogLogger;
 import org.signalml.plugin.export.Plugin;
 import org.signalml.plugin.export.PluginAuth;
@@ -12,11 +14,32 @@ import org.signalml.plugin.export.SvarogAccess;
  * @author Stanislaw Findeisen (Eisenbits)
  */
 public class SFTestPlugin implements Plugin {
+    private SvarogAccess svarogAccess;
+    private PluginAuth pluginAuth;
+
+    private void testI18nPlural(int k) {
+        String sraw= svarogAccess.getI18nAccess().translateN(pluginAuth, "I18nBundle", "Deleted one file", "Deleted {0} files", k);
+        SvarogLogger.getSharedInstance().debug("SFTestPlugin.testI18nPlural(): raw=" + sraw);
+        String msg = MessageFormat.format(sraw, k);
+        SvarogLogger.getSharedInstance().debug("SFTestPlugin.testI18nPlural(): msg=" + msg);
+    }
+
 	public void register(SvarogAccess sa, PluginAuth auth) throws SignalMLException {
-	    SvarogLogger.getSharedInstance().debug("SFTestPlugin.register(): hello world!");
+	    this.svarogAccess = sa;
+	    this.pluginAuth = auth;
+
+	    SvarogLogger.getSharedInstance().debug("SFTestPlugin.register()");
 	    sa.getGUIAccess().addButtonToToolsMenu(new BombAction());
 	    sa.getGUIAccess().addButtonToToolsMenu(new GrenadeAction());
 	    new Thread(new ClassLoaderTest(15000)).start();
         new Thread(new ClockBomb(30000)).start();
+
+        String s1 = sa.getI18nAccess().translate(auth, "I18nBundle", "This is an i18n test. Hello, world!");
+        SvarogLogger.getSharedInstance().debug("SFTestPlugin.register(): " + s1);
+
+        testI18nPlural(0);
+        testI18nPlural(1);
+        testI18nPlural(4);
+        testI18nPlural(5);
 	}
 }
