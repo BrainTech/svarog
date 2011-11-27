feb503c48904cfd88549f759c1cb7def12f7218f
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2011-11-27 22:23:48 +0100

    Renamed the montage generators files to *_gen.xml.

--- svarog/src/main/java/org/signalml/app/config/preset/MontageGeneratorsPresetManager.java
+++ svarog/src/main/java/org/signalml/app/config/preset/MontageGeneratorsPresetManager.java
@@ -21,7 +21,7 @@ public class MontageGeneratorsPresetManager extends AbstractMultifileRestoreable
 
 	@Override
 	public String[] getDefaultFileNames() {
-		return new String[]{"eeg10_20_cap19.xml", "eeg10_10_cap33.xml", "eeg10_10_cap47.xml"};
+		return new String[]{"eeg10_20_cap19_gen.xml", "eeg10_10_cap33_gen.xml", "eeg10_10_cap47_gen.xml"};
 	}
 
 	@Override
