2ed8e8526904c971519f64aaea578397ada3a819
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-12-05 09:33:37 +0100

    Revert "Use reflection to access configuration variables"
    This reverts commit 10781855283550dc506b6e41c86306c25372037e.

--- src/main/java/org/signalml/app/config/ApplicationConfiguration.java
+++ src/main/java/org/signalml/app/config/ApplicationConfiguration.java
@@ -5,8 +5,6 @@
 package org.signalml.app.config;
 
 import java.io.File;
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
 import javax.swing.ToolTipManager;
 
 import org.signalml.app.method.artifact.ArtifactConfiguration;
@@ -687,19 +685,14 @@ public class ApplicationConfiguration extends AbstractXMLConfiguration {
 	 * @param name name is the same as function name without get
 	 *
 	 */
-	public String getPath(String name) {
-		try {
-			Method method = this.getClass().getDeclaredMethod("get" + name);
-			return (String)method.invoke(this);
-		// all invocation exceptions are converted to runtime exception
-		// and signify programming mistakes.
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		} catch (InvocationTargetException e) {
-			throw new RuntimeException(e);
-		}
+	public String getPath(String name){
+		if(name.equals("LastConsoleSaveAsTextPath"))
+			return this.getLastConsoleSaveAsTextPath();
+		if(name.equals("LastSamplesSaveAsTextPath"))
+			return this.getLastSamplesSaveAsTextPath();
+		if(name.equals("LastTableSaveAsTextPath"))
+			return this.getLastTableSaveAsTextPath();
+		throw new AssertionError("unknown configuration path name");
 	}
 
 	/**
@@ -709,18 +702,8 @@ public class ApplicationConfiguration extends AbstractXMLConfiguration {
 	 *
 	 */
 	public void setPath(String name, String path){
-		try {
-			name = "set" + name;
-			Method method = this.getClass().getDeclaredMethod(name, String.class);
-			method.invoke(this, path);
-		// all invocation exceptions are converted to runtime exception
-		// and signify programming mistakes.
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		} catch (InvocationTargetException e) {
-			throw new RuntimeException(e);
-		}
+		if(name.equals("LastSamplesSaveAsTextPath"))
+			this.setLastSamplesSaveAsTextPath(path);
+		throw new AssertionError("unknown configuration path name");
 	}
 }
