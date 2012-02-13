2ffc085f28adab701b067ab0aea0f3d31f599ae8
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2012-02-13 21:03:54 +0100

    Added the checkedSrvs field to the FindEEGExperimentsRequest.

--- svarog/src/main/java/org/signalml/app/worker/monitor/messages/FindEEGExperimentsRequest.java
+++ svarog/src/main/java/org/signalml/app/worker/monitor/messages/FindEEGExperimentsRequest.java
@@ -7,9 +7,13 @@ public class FindEEGExperimentsRequest extends Message {
 	@JsonProperty("client_push_address")
 	private String clientPushAddress;
 	
+	@JsonProperty("checked_srvs")
+	private String checkedSrvs;
+
 	public FindEEGExperimentsRequest(String clientPushAddress) {
 		super(MessageType.FIND_EEG_EXPERIMENTS_REQUEST);
 		this.clientPushAddress = clientPushAddress;
+		this.checkedSrvs = "";
 	}
 
 	public String getClientPushAddress() {
