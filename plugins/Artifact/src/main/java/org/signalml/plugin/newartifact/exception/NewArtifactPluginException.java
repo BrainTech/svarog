bb538c54402de77756d5dda3faa4c5f5f6a0a745
Konrad Durnoga <kdr@mimuw.edu.pl>
2012-02-23 02:17:29 +0100

    Replaced string channel names with enum values

---
+++ plugins/Artifact/src/main/java/org/signalml/plugin/newartifact/exception/NewArtifactPluginException.java
@@ -0,0 +1,28 @@
+package org.signalml.plugin.newartifact.exception;
+
+import org.signalml.plugin.exception.PluginException;
+
+public class NewArtifactPluginException extends PluginException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7907800548785251685L;
+
+	public NewArtifactPluginException() {
+		super();
+	}
+
+	public NewArtifactPluginException(String message) {
+		super(message);
+	}
+
+	public NewArtifactPluginException(Throwable cause) {
+		super(cause);
+	}
+
+	public NewArtifactPluginException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
