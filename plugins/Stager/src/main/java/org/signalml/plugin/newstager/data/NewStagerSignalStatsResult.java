cf03d486cf3c38e6167c267eccfc14a22114d0b6
Konrad Durnoga <kdr@mimuw.edu.pl>
2012-03-15 22:33:30 +0100

    Added some serial ids

--- plugins/Stager/src/main/java/org/signalml/plugin/newstager/data/NewStagerSignalStatsResult.java
+++ plugins/Stager/src/main/java/org/signalml/plugin/newstager/data/NewStagerSignalStatsResult.java
@@ -2,6 +2,9 @@ package org.signalml.plugin.newstager.data;
 
 public class NewStagerSignalStatsResult extends NewStagerResult { //TODO this should not extend NewStagerResult
 
+	private static final long serialVersionUID = -3346713217515671076L;
+	
+	
 	public final NewStagerSleepStats signalStatCoeffs;
 	public final NewStagerParameters newParameters;
 	public final double muscle[];
--- plugins/Stager/src/main/java/org/signalml/plugin/newstager/data/logic/NewStagerBookProcessorStepResult.java
+++ plugins/Stager/src/main/java/org/signalml/plugin/newstager/data/logic/NewStagerBookProcessorStepResult.java
@@ -7,6 +7,9 @@ import org.signalml.plugin.newstager.data.NewStagerResult;
 
 public class NewStagerBookProcessorStepResult extends NewStagerResult {
 
+	private static final long serialVersionUID = -6450518358699779057L;
+	
+	
 	public final NewStagerBookInfo bookInfo;
 	public final Collection<NewStagerBookProcessorResult> tagResults;
 	public final boolean montage[];
