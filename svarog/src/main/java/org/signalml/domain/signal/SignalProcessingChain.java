BrainTech/svarog

commit cc2ee4b3ef8768e8575971a59618aa8cdb688702

Changed SignalMLMRUDEntry to use SignalMLDescriptor.
Previously the signal parameters of a previously opened
SignalML file was contained in the MRUD entry as separate
fields instead of aggregating them into a SignalParameters
class.

svarog/src/main/java/org/signalml/domain/signal/SignalProcessingChain.java
@@ -18,6 +18,9 @@ import org.signalml.app.document.RawSignalDocument;
 import org.signalml.app.document.RawSignalMRUDEntry;
 import org.signalml.app.document.SignalMLDocument;
 import org.signalml.app.document.SignalMLMRUDEntry;
+import org.signalml.app.model.document.opensignal.AbstractOpenSignalDescriptor;
+import org.signalml.app.model.document.opensignal.SignalMLDescriptor;
+import org.signalml.app.model.document.opensignal.elements.SignalParameters;
 import org.signalml.codec.SignalMLCodec;
 import org.signalml.codec.SignalMLCodecReader;
 import org.signalml.domain.montage.Montage;
@@ -113,9 +116,10 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 		if (mrud instanceof SignalMLMRUDEntry) {
 
 			SignalMLMRUDEntry smlEntry = (SignalMLMRUDEntry) mrud;
-			SignalMLCodec codec = SvarogApplication.getSharedInstance().getSignalMLCodecManager().getCodecByUID(smlEntry.getCodecUID());
+			String codecUID = smlEntry.getDescriptor().getCodecUID();
+			SignalMLCodec codec = SvarogApplication.getSharedInstance().getSignalMLCodecManager().getCodecByUID(codecUID);
 			if (codec == null) {
-				logger.warn("Mrud codec not found for uid [" + smlEntry.getCodecUID() + "]");
+				logger.warn("Mrud codec not found for uid [" + codecUID + "]");
 				throw new MissingCodecException("error.mrudMissingCodecException");
 			}
 
@@ -124,14 +128,15 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 
 			source = new SignalMLCodecSampleSource(reader);
 
+			SignalParameters signalParameters = smlEntry.getDescriptor().getSignalParameters();
 			if (source.isCalibrationCapable()) {
-				source.setCalibrationGain(smlEntry.getCalibrationGain());
+				source.setCalibrationGain(signalParameters.getCalibrationGain());
 			}
 			if (!source.isSamplingFrequencyCapable()) {
-				source.setSamplingFrequency(smlEntry.getSamplingFrequency());
+				source.setSamplingFrequency(signalParameters.getSamplingFrequency());
 			}
 			if (!source.isChannelCountCapable()) {
-				source.setChannelCount(smlEntry.getChannelCount());
+				source.setChannelCount(signalParameters.getChannelCount());
 			}
 
 		}
@@ -642,19 +647,23 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 		}
 		descriptor.setSourceBuffered(sourceBuffer != null);
 
+		SignalParameters signalParameters = new SignalParameters();
+		signalParameters.setPageSize(-1F);
+		signalParameters.setBlocksPerPage(-1);
+		signalParameters.setSamplingFrequency(source.getSamplingFrequency());
+		signalParameters.setChannelCount(source.getChannelCount());
+		signalParameters.setCalibrationGain(source.getCalibrationGain());
+		
 		if (source instanceof SignalMLCodecSampleSource) {
 
 			SignalMLCodecSampleSource codecSource = (SignalMLCodecSampleSource) source;
 			SignalMLCodecReader reader = codecSource.getReader();
-			SignalMLCodec codec = reader.getCodec();
+			
+			SignalMLDescriptor signalMLDescriptor = new SignalMLDescriptor();
+			signalMLDescriptor.setSignalParameters(signalParameters);
 
-			SignalMLMRUDEntry mrud = new SignalMLMRUDEntry(ManagedDocumentType.SIGNAL, SignalMLDocument.class, reader.getCurrentFilename(), codec.getSourceUID(), codec.getFormatName());
+			SignalMLMRUDEntry mrud = new SignalMLMRUDEntry(ManagedDocumentType.SIGNAL, SignalMLDocument.class, reader.getCurrentFilename(), signalMLDescriptor);
 			mrud.setLastTimeOpened(new Date());
-			mrud.setPageSize(-1F);
-			mrud.setBlocksPerPage(-1);
-			mrud.setSamplingFrequency(codecSource.getSamplingFrequency());
-			mrud.setChannelCount(codecSource.getChannelCount());
-			mrud.setCalibrationGain(codecSource.getSingleCalibrationGain());
 
 			descriptor.setDocument(mrud);
 
@@ -663,13 +672,8 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 
 			RawSignalSampleSource rawSource = (RawSignalSampleSource) source;
 			RawSignalDescriptor rawDescriptor = new RawSignalDescriptor();
-
-			rawDescriptor.setSamplingFrequency(rawSource.getSamplingFrequency());
-			rawDescriptor.setSampleCount(rawSource.getSampleCount());
-			rawDescriptor.setChannelCount(rawSource.getChannelCount());
-			rawDescriptor.setCalibrationGain(rawSource.getCalibrationGain());
-			rawDescriptor.setSampleType(rawSource.getSampleType());
-			rawDescriptor.setByteOrder(rawSource.getByteOrder());
+			
+			rawDescriptor.setSignalParameters(signalParameters);
 
 			RawSignalMRUDEntry mrud = new RawSignalMRUDEntry(ManagedDocumentType.SIGNAL, RawSignalDocument.class, rawSource.getFile().getAbsolutePath(), rawDescriptor);
 			mrud.setLastTimeOpened(new Date());
@@ -681,6 +685,7 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 			throw new SanityCheckException("Unsupported sample source type: " + source.getClass().getName());
 
 		}
+
 
 		return descriptor;
 

svarog/src/main/java/org/signalml/app/SvarogApplication.java
@@ -57,6 +57,7 @@ import org.signalml.app.method.example.ExampleMethodDescriptor;
 import org.signalml.app.method.mp5.MP5ApplicationData;
 import org.signalml.app.method.mp5.MP5ExecutorManager;
 import org.signalml.app.method.mp5.MP5MethodDescriptor;
+import org.signalml.app.model.document.opensignal.SignalMLDescriptor;
 import org.signalml.app.model.montage.MontagePresetManager;
 import org.signalml.app.task.ApplicationTaskManager;
 import org.signalml.app.util.MatlabUtil;
@@ -641,6 +642,7 @@ public class SvarogApplication implements java.lang.Runnable {
 					     SignalMLCodecDescriptor.class,
 					     MRUDConfiguration.class,
 					     MRUDEntry.class,
+					     SignalMLDescriptor.class,
 					     SignalMLMRUDEntry.class,
 					     RawSignalMRUDEntry.class,
 					     RawSignalDescriptor.class,

svarog/src/main/java/org/signalml/app/config/WorkspaceSignal.java
@@ -26,6 +26,7 @@ import org.signalml.app.document.SignalDocument;
 import org.signalml.app.document.SignalMLDocument;
 import org.signalml.app.document.SignalMLMRUDEntry;
 import org.signalml.app.document.TagDocument;
+import org.signalml.app.model.document.opensignal.SignalMLDescriptor;
 import org.signalml.app.util.SnapToPageRunnable;
 import org.signalml.app.view.components.LockableJSplitPane;
 import org.signalml.app.view.signal.SignalPlot;
@@ -80,14 +81,9 @@ public class WorkspaceSignal extends WorkspaceDocument {
 
 			SignalMLDocument signalMLDocument = (SignalMLDocument) document;
 
-			SignalMLCodec codec = signalMLDocument.getReader().getCodec();
-			SignalMLMRUDEntry mrud = new SignalMLMRUDEntry(ManagedDocumentType.SIGNAL, signalMLDocument.getClass(), ((FileBackedDocument) document).getBackingFile().getAbsolutePath(), codec.getSourceUID(), codec.getFormatName());
+			SignalMLDescriptor descriptor = new SignalMLDescriptor(signalMLDocument);
+			SignalMLMRUDEntry mrud = new SignalMLMRUDEntry(ManagedDocumentType.SIGNAL, signalMLDocument.getClass(), ((FileBackedDocument) document).getBackingFile().getAbsolutePath(), descriptor);
 			mrud.setLastTimeOpened(new Date());
-			mrud.setPageSize(signalMLDocument.getPageSize());
-			mrud.setBlocksPerPage(signalMLDocument.getBlocksPerPage());
-			mrud.setSamplingFrequency(signalMLDocument.getSamplingFrequency());
-			mrud.setChannelCount(signalMLDocument.getChannelCount());
-			mrud.setCalibrationGain(signalMLDocument.getCalibration());
 
 			mrudEntry = mrud;
 

svarog/src/main/java/org/signalml/app/document/AbstractSignal.java
@@ -266,6 +266,13 @@ public abstract class AbstractSignal extends AbstractDocument implements SignalD
 		}
 		return montage;
 	}
+	
+	@Override
+	public boolean isMontageCreated() {
+		if (montage == null) 
+			return false;
+		return true;
+	}
 
 	protected Montage createDefaultMontage() {
 		return SignalConfigurer.createMontage(this);

svarog/src/main/java/org/signalml/app/document/DocumentFlowIntegrator.java
@@ -574,23 +574,16 @@ public class DocumentFlowIntegrator {
 			if (mrud instanceof SignalMLMRUDEntry) {
 
 				SignalMLMRUDEntry smlEntry = (SignalMLMRUDEntry) mrud;
-				SignalMLCodec codec = codecManager.getCodecByUID(smlEntry.getCodecUID());
+				SignalMLDescriptor signalmlDescriptor = smlEntry.getDescriptor();
+				
+				SignalMLCodec codec = codecManager.getCodecByUID(signalmlDescriptor.getCodecUID());
+				
 				if (codec == null) {
-					logger.warn("Mrud codec not found for uid [" + smlEntry.getCodecUID() + "]");
+					logger.warn("Mrud codec not found for uid [" + signalmlDescriptor.getCodecUID() + "]");
 					throw new MissingCodecException("error.mrudMissingCodecException");
 				}
-
-				SignalMLDescriptor signalmlDescriptor = new SignalMLDescriptor();
 				signalmlDescriptor.setCodec(codec);
 				odd.setOpenSignalDescriptor(signalmlDescriptor);
-
-				SignalParameters spd = signalmlDescriptor.getSignalParameters();
-				spd.setPageSize(smlEntry.getPageSize());
-				spd.setBlocksPerPage(smlEntry.getBlocksPerPage());
-				spd.setSamplingFrequency(smlEntry.getSamplingFrequency());
-				spd.setChannelCount(smlEntry.getChannelCount());
-				spd.setCalibrationGain(new float[] {smlEntry.getCalibrationGain()});
-
 			}
 			else if (mrud instanceof RawSignalMRUDEntry) {
 
@@ -737,13 +730,8 @@ public class DocumentFlowIntegrator {
 			}
 
 			if (mrudRegistry != null) {
-				SignalMLMRUDEntry mrud = new SignalMLMRUDEntry(ManagedDocumentType.SIGNAL, signalMLDocument.getClass(), file.getAbsolutePath(), codec.getSourceUID(), codec.getFormatName());
+				SignalMLMRUDEntry mrud = new SignalMLMRUDEntry(ManagedDocumentType.SIGNAL, signalMLDocument.getClass(), file.getAbsolutePath(), signalMLDescriptor);
 				mrud.setLastTimeOpened(new Date());
-				mrud.setPageSize(spd.getPageSize());
-				mrud.setBlocksPerPage(spd.getBlocksPerPage());
-				mrud.setSamplingFrequency(spd.getSamplingFrequency());
-				mrud.setChannelCount(spd.getChannelCount());
-				mrud.setCalibrationGain(spd.getCalibrationGain() == null ? null : spd.getCalibrationGain()[0]);
 				mrudRegistry.registerMRUDEntry(mrud);
 			}
 

svarog/src/main/java/org/signalml/app/document/SignalDocument.java
@@ -154,6 +154,8 @@ public interface SignalDocument extends ExportedSignalDocument {
 	 */
 	Montage getMontage();
 	
+	boolean isMontageCreated();
+	
 	/**
 	 * Sets the {@link Montage} montage for the signal in this document.
 	 * @param montage montage for the signal in this document

svarog/src/main/java/org/signalml/app/document/SignalMLMRUDEntry.java
@@ -4,18 +4,14 @@
 
 package org.signalml.app.document;
 
-import java.beans.IntrospectionException;
-import java.util.List;
-
-import org.signalml.app.model.components.LabelledPropertyDescriptor;
+import org.signalml.app.model.document.opensignal.SignalMLDescriptor;
 import org.signalml.codec.SignalMLCodec;
-import org.signalml.plugin.export.signal.Document;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
- * Serializable description of a file with a signal stored in it using
- * signalML {@link SignalMLCodec codec}. Contains:
+ * Serializable description of a file with a signal stored in it using signalML
+ * {@link SignalMLCodec codec}. Contains:
  * <ul>
  * <li>name and id of the codec</li>
  * <li>sampling frequency of the signal</li>
@@ -24,197 +20,29 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
  * single page</li>
  * <li>calibration of the signal</li>
  * </ul>
- *
- * @author Michal Dobaczewski &copy; 2007-2008 CC Otwarte Systemy Komputerowe Sp. z o.o.
+ * 
+ * @author Michal Dobaczewski &copy; 2007-2008 CC Otwarte Systemy Komputerowe
+ *         Sp. z o.o.
  */
 @XStreamAlias("mrud-signalml")
 public class SignalMLMRUDEntry extends MRUDEntry {
 
-	/**
-	 * unique identifier of the {@link SignalMLCodec codec}
-	 */
-	private String codecUID;
-	
-	/**
-	 * the format name of the {@link SignalMLCodec codec}
-	 */
-	private String formatName;
+	private SignalMLDescriptor descriptor;
 
-	/**
-	 * the number of samples in one second of the signal (in a single channel)
-	 */
-	private Float samplingFrequency;
-	
-	/**
-	 * the number of channels in the signal
-	 */
-	private Integer channelCount;
-	
-	/**
-	 * the value of calibration
-	 */
-	private Float calibration;
-	
-	/**
-	 * the size of a page of the signal in seconds
-	 */
-	private Float pageSize;
-	
-	/**
-	 * the number of blocks in a single page of the signal
-	 */
-	private Integer blocksPerPage;
-
-	/**
-	 * Empty constructor.
-	 */
-	protected SignalMLMRUDEntry() {
-		super();
-	}
-
-	/**
-	 * Constructor. Sets:
-	 * <ul>
-	 * <li>the type of {@link ManagedDocumentType type} of the {@link Document
-	 * document},</li>
-	 * <li>the class of the document,</li>
-	 * <li>the new file created on the basis of the provided path,</li>
-	 * <li>the path to the file converted to the absolute path,</li>
-	 * <li>the unique identifier of the {@link SignalMLCodec codec},</li>
-	 * <li>the format name of the codec.</li>
-	 * </ul>
-	 * @param documentType the type of type of the document
-	 * @param documentClass the class of the document
-	 * @param path the path to the file
-	 * @param codecUID the unique identifier of the codec
-	 * @param formatName the format name of the codec
-	 */
-	public SignalMLMRUDEntry(ManagedDocumentType documentType, Class<?> documentClass, String path, String codecUID, String formatName) {
+	public SignalMLMRUDEntry(ManagedDocumentType documentType, Class<?> documentClass, String path, SignalMLDescriptor descriptor) {
 		super(documentType, documentClass, path);
-		this.codecUID = codecUID;
-		this.formatName = formatName;
+		this.descriptor = descriptor;
 	}
 
-	/**
-	 * Returns the unique identifier of the {@link SignalMLCodec codec}.
-	 * @return the unique identifier of the codec
-	 */
-	public String getCodecUID() {
-		return codecUID;
+	public SignalMLMRUDEntry() {
 	}
 
-	/**
-	 * Sets the unique identifier of the {@link SignalMLCodec codec}.
-	 * @param codecUID the unique identifier of the codec
-	 */
-	public void setCodecUID(String codecUID) {
-		this.codecUID = codecUID;
+	public SignalMLDescriptor getDescriptor() {
+		return descriptor;
 	}
 
-	/**
-	 * Returns the format name of the {@link SignalMLCodec codec}.
-	 * @return the format name of the codec
-	 */
-	public String getFormatName() {
-		return formatName;
-	}
-
-	/**
-	 * Sets the format name of the {@link SignalMLCodec codec}.
-	 * @param formatName the format name of the codec
-	 */
-	public void setFormatName(String formatName) {
-		this.formatName = formatName;
-	}
-
-	/**
-	 * Returns the number of samples per second in a single channel.
-	 * @return the sampling frequency
-	 */
-	public Float getSamplingFrequency() {
-		return samplingFrequency;
-	}
-
-	/**
-	 * Sets the number of samples per second in a single channel.
-	 * @param samplingFrequency the sampling frequency
-	 */
-	public void setSamplingFrequency(Float samplingFrequency) {
-		this.samplingFrequency = samplingFrequency;
-	}
-
-	/**
-	 * Returns the number of channels in the signal.
-	 * @return the number of channels in the signal
-	 */
-	public Integer getChannelCount() {
-		return channelCount;
-	}
-
-	/**
-	 * Sets the number of channels in the signal.
-	 * @param channelCount the number of channels in the signal
-	 */
-	public void setChannelCount(Integer channelCount) {
-		this.channelCount = channelCount;
-	}
-
-	/**
-	 * Returns the value of calibration.
-	 * @return the value of calibration
-	 */
-	public Float getCalibrationGain() {
-		return calibration;
-	}
-
-	/**
-	 * Sets the value of calibration.
-	 * @param calibration the value of calibration
-	 */
-	public void setCalibrationGain(Float calibration) {
-		this.calibration = calibration;
-	}
-
-	/**
-	 * Returns the size (length) of a page of signal in seconds.
-	 * @return the size (length) of a page
-	 */
-	public Float getPageSize() {
-		return pageSize;
-	}
-
-	/**
-	 * Sets the size (length) of a page of signal in seconds.
-	 * @param pageSize the size (length) of a page
-	 */
-	public void setPageSize(Float pageSize) {
-		this.pageSize = pageSize;
-	}
-
-	/**
-	 * Returns the number of blocks in a single page of the signal.
-	 * @return the number of blocks in a single page of the signal
-	 */
-	public Integer getBlocksPerPage() {
-		return blocksPerPage;
-	}
-
-	/**
-	 * Sets the number of blocks in a single page of the signal.
-	 * @param blocksPerPage the number of blocks in a single page of the signal
-	 */
-	public void setBlocksPerPage(Integer blocksPerPage) {
-		this.blocksPerPage = blocksPerPage;
-	}
-
-	@Override
-	public List<LabelledPropertyDescriptor> getPropertyList() throws IntrospectionException {
-		List<LabelledPropertyDescriptor> list = super.getPropertyList();
-
-		list.add(2, new LabelledPropertyDescriptor("property.signalmlmrud.formatName", "formatName", SignalMLMRUDEntry.class));
-
-		return list;
-
+	public void setDescriptor(SignalMLDescriptor descriptor) {
+		this.descriptor = descriptor;
 	}
 
 }

svarog/src/main/java/org/signalml/app/model/document/opensignal/SignalMLDescriptor.java
@@ -1,10 +1,11 @@
 package org.signalml.app.model.document.opensignal;
 
+import org.signalml.app.document.SignalMLDocument;
 import org.signalml.codec.SignalMLCodec;
 
 /**
  * Describes the parameters of a SignalML file to be opened.
- *
+ * 
  * @author Piotr Szachewicz
  */
 public class SignalMLDescriptor extends AbstractOpenSignalDescriptor {
@@ -12,7 +13,32 @@ public class SignalMLDescriptor extends AbstractOpenSignalDescriptor {
 	/**
 	 * The codec that will be used to open the signalML file.
 	 */
-	private SignalMLCodec codec;
+	private transient SignalMLCodec codec;
+
+	private String formatName;
+	private String codecUID;
+
+	public SignalMLDescriptor() {
+	}
+
+	public SignalMLDescriptor(SignalMLDocument signalMLDocument) {
+		int channelCount = signalMLDocument.getChannelCount();
+		float samplingFrequency = signalMLDocument.getSamplingFrequency();
+
+		String[] channelLabels = new String[channelCount];
+		for (int i = 0; i < channelCount; i++) {
+			channelLabels[i] = signalMLDocument.getSampleSource().getLabel(i);
+		}
+
+		setChannelLabels(channelLabels);
+		signalParameters.setChannelCount(channelCount);
+		signalParameters.setSamplingFrequency(samplingFrequency);
+
+		setMontage(signalMLDocument.getMontage());
+		setCodec(signalMLDocument.getReader().getCodec());
+
+		// TODO gain/offset
+	}
 
 	public SignalMLCodec getCodec() {
 		return codec;
@@ -20,5 +46,21 @@ public class SignalMLDescriptor extends AbstractOpenSignalDescriptor {
 
 	public void setCodec(SignalMLCodec codec) {
 		this.codec = codec;
+		if (codec == null) {
+			formatName = "";
+			codecUID = "";
+		} else {
+			formatName = codec.getFormatName();
+			codecUID = codec.getSourceUID();
+		}
 	}
+
+	public String getFormatName() {
+		return formatName;
+	}
+
+	public String getCodecUID() {
+		return codecUID;
+	}
+
 }

commit f4ac5a7b8d8a7445056b1f0feb49f5e73c566f78

Some fields were not set while creating a SignalProcessingChainDescriptor.
This caused a NullPointerException while restarting Svarog after
performing an MP5 algorithm.
(NullPointerException in the RawSignalSampleSource constructor).
This bug was introduced in commit cc2ee4b3.

svarog/src/main/java/org/signalml/domain/signal/SignalProcessingChain.java
@@ -26,6 +26,7 @@ import org.signalml.codec.SignalMLCodecReader;
 import org.signalml.domain.montage.Montage;
 import org.signalml.domain.montage.MontageChannel;
 import org.signalml.domain.montage.MontageMismatchException;
+import org.signalml.domain.signal.raw.RawSignalByteOrder;
 import org.signalml.domain.signal.raw.RawSignalDescriptor;
 import org.signalml.domain.signal.raw.RawSignalSampleSource;
 import org.signalml.domain.signal.space.SignalSourceLevel;
@@ -653,6 +654,7 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 		signalParameters.setSamplingFrequency(source.getSamplingFrequency());
 		signalParameters.setChannelCount(source.getChannelCount());
 		signalParameters.setCalibrationGain(source.getCalibrationGain());
+		signalParameters.setChannelCount(source.getChannelCount());
 		
 		if (source instanceof SignalMLCodecSampleSource) {
 
@@ -674,6 +676,9 @@ public class SignalProcessingChain extends AbstractMultichannelSampleSource impl
 			RawSignalDescriptor rawDescriptor = new RawSignalDescriptor();
 			
 			rawDescriptor.setSignalParameters(signalParameters);
+			rawDescriptor.setSampleCount(rawSource.getSampleCount());
+			rawDescriptor.setSampleType(rawSource.getSampleType());
+			rawDescriptor.setByteOrder(rawSource.getByteOrder());
 
 			RawSignalMRUDEntry mrud = new RawSignalMRUDEntry(ManagedDocumentType.SIGNAL, RawSignalDocument.class, rawSource.getFile().getAbsolutePath(), rawDescriptor);
 			mrud.setLastTimeOpened(new Date());