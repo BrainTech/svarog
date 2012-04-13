a82c066ea12f33e9b0ac29e346ef3aa8acbfc69e
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-04-13 04:57:44 +0200

    Reindent .xml files to HT=4 SP
    Done with:
      dos2unix $(git ls-files \*.xml)
      sed -i -r 's/\s+$//' $(git ls-files \*.xml)
      sed -i -r 's/\t/    /g' $(git ls-files \*.xml)
    
    XML files tend to have many level, so indenting them 8 spaces was a real PITA.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,8 +2,8 @@
          (tab-width . 4)
          (fill-column . 100)))
  (java-mode . ((c-basic-offset . 4)))
- (sgml-mode . ((sgml-basic-offset . 8)))
- (nxml-mode . ((nxml-child-indent . 8)))
+ (sgml-mode . ((sgml-basic-offset . 4)))
+ (nxml-mode . ((nxml-child-indent . 4)))
  ; emacs complains about sgml-basic-offset being unsafe, so let's set it only for XML
  (emacs-lisp-mode . ((indent-tabs-mode . nil)))
 )
