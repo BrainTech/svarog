79da678c03cdbe6a088e36072a8444e2a51467e2
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2012-04-26 15:39:53 +0200

    Shortened the message in the profile path selection dialog.
    The former message was too long and complicated,
    probably no one has ever read it ;). Trac ticket #59.

--- svarog/src/main/java/org/signalml/app/view/components/ProfilePathTypePanel.java
+++ svarog/src/main/java/org/signalml/app/view/components/ProfilePathTypePanel.java
@@ -11,7 +11,6 @@ import java.awt.Font;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
@@ -95,7 +94,7 @@ public class ProfilePathTypePanel extends JPanel {
 	 */
 	private void initialize() {
 
-		setBorder(BorderFactory.createTitledBorder(_("Choose profile path")));
+		setBorder(BorderFactory.createTitledBorder(""));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 		welcomeLabel = new JLabel();
@@ -129,7 +128,7 @@ public class ProfilePathTypePanel extends JPanel {
 	public JRadioButton getDefaultRadio() {
 		if (defaultRadio == null) {
 			defaultRadio = new JRadioButton();
-			defaultRadio.setText(_("signalml directory inside default home"));
+			defaultRadio.setText(_("signalml directory inside the user home directory (preferred)"));
 			defaultRadio.setAlignmentX(Component.LEFT_ALIGNMENT);
 			radioGroup.add(defaultRadio);
 		}
@@ -178,7 +177,9 @@ public class ProfilePathTypePanel extends JPanel {
 		if (infoLabel == null) {
 			infoLabel = new JLabel();
 			infoLabel.setFont(infoLabel.getFont().deriveFont(Font.PLAIN, 12));
-			infoLabel.setText("<html><body><div style=\"width: 400px; text-align: justify;\">" + _("The signalml viewer needs a directory to store its configuration and files. This directory may reside inside the default user's home directory (system dependent), or you can select any directory on disk (remember that you need to select a concrete directory for SignalML files, not the directory in which you would like the later to be created - create the directory if necessary).") + "</div></body></html>");
+			infoLabel.setText("<html><body><div style=\"width: 400px; text-align: justify;\">" +
+			_("Please choose a directory in which Svarog can store its configuration files.") +
+			 "</div></body></html>");
 			infoLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 //			infoLabel.setPreferredSize(new Dimension(500,90));
 			infoLabel.setBorder(new EmptyBorder(3,0,3,0));
