BrainTech/svarog

commit 9fdff9d9d9c0a401085a05079b3fa5fee637fd56

RawSignalDescriptor extends AbstractOpenSignalDescriptor.
That means that RawSignalDescriptor uses the signal
parameters that are defined in the
AbstractOpenSignalDescriptor.

svarog/src/main/java/org/signalml/domain/signal/raw/RawSignalDescriptor.java
@@ -8,6 +8,8 @@ import java.util.Date;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import java.util.Arrays;
+
+import org.signalml.app.model.document.opensignal.AbstractOpenSignalDescriptor;
 import org.signalml.domain.montage.system.EegSystem;
 import org.signalml.domain.montage.system.EegSystemName;
 
@@ -21,7 +23,7 @@ import org.signalml.domain.montage.system.EegSystemName;
  * @author Michal Dobaczewski &copy; 2007-2008 CC Otwarte Systemy Komputerowe Sp. z o.o.
  */
 @XStreamAlias("rawdescriptor")
-public class RawSignalDescriptor {
+public class RawSignalDescriptor extends AbstractOpenSignalDescriptor {
 
         /**
          * This enumerator tells if the signal is stored as raw or described
@@ -64,50 +66,15 @@ public class RawSignalDescriptor {
 	private String sourceSignalMLSourceUID;
 
         /**
-         * the number of samples per second
-         */
-	private float samplingFrequency;
-        /**
-         * the number of channels in the signal
-         */
-	private int channelCount;
-        /**
          * the number of samples in a single channel
          */
 	private int sampleCount;
-
-	/**
-	 * The calibration gain for the signal - the value by which each sample
-	 * value is multiplied.
-	 */
-	private float[] calibrationGain;
-
-	/**
-	 * The calibration offset for the signal - the value which is added
-	 * to each sample value.
-	 */
-	private float[] calibrationOffset;
-	private Float minimumValue;
-	private Float maximumValue;
 
         /**
          * the {@link RawSignalSampleType type} of the samples
          */
 	private RawSignalSampleType sampleType;
-        /**
-         * the {@link RawSignalByteOrder order} of bytes in the file with
-         * the signal
-         */
 	private RawSignalByteOrder byteOrder;
-
-        /**
-         * the length of the page in seconds
-         */
-	private float pageSize;
-        /**
-         * the number of blocks in one page
-         */
-	private int blocksPerPage;
 
 	/**
 	 * The name of the {@link EegSystem EEG system} that is used
@@ -251,7 +218,7 @@ public class RawSignalDescriptor {
          * @return the number of samples per second
          */
 	public float getSamplingFrequency() {
-		return samplingFrequency;
+		return signalParameters.getSamplingFrequency();
 	}
 
         /**
@@ -259,7 +226,7 @@ public class RawSignalDescriptor {
          * @param samplingFrequency the number of samples per second
          */
 	public void setSamplingFrequency(float samplingFrequency) {
-		this.samplingFrequency = samplingFrequency;
+		signalParameters.setSamplingFrequency(samplingFrequency);
 	}
 
         /**
@@ -267,7 +234,7 @@ public class RawSignalDescriptor {
          * @return the number of channels in the signal
          */
 	public int getChannelCount() {
-		return channelCount;
+		return signalParameters.getChannelCount();
 	}
 
         /**
@@ -275,7 +242,7 @@ public class RawSignalDescriptor {
          * @param channelCount the number of channels in the signal
          */
 	public void setChannelCount(int channelCount) {
-		this.channelCount = channelCount;
+		signalParameters.setChannelCount(channelCount);
 	}
 
         /**
@@ -295,10 +262,11 @@ public class RawSignalDescriptor {
 	}
 	
 	public float[] getCalibrationGain() {
-		return calibrationGain;
+		return signalParameters.getCalibrationGain();
 	}
 
 	public void setCalibrationGain(float calibration) {
+		float[] calibrationGain = signalParameters.getCalibrationGain();
 		if (calibrationGain == null || getChannelCount() != calibrationGain.length) {
 			if (getChannelCount() > 0)
 				calibrationGain = new float[getChannelCount()];
@@ -306,29 +274,32 @@ public class RawSignalDescriptor {
 				calibrationGain = new float[1];
 		}
 		Arrays.fill(calibrationGain, calibration);
+		signalParameters.setCalibrationGain(calibrationGain);
 	}
 
 	public void setCalibrationGain(float[] calibrationGain) {
-		this.calibrationGain = calibrationGain;
+		signalParameters.setCalibrationGain(calibrationGain);
 	}
 
 	public float[] getCalibrationOffset() {
-		return calibrationOffset;
+		return signalParameters.getCalibrationOffset();
 	}
 
 	/**
 	 * Sets a value of calibration offset for this signal (same for every
 	 * channel.
-	 * @param calibrationOffset new value of calibration offset for
+	 * @param offset new value of calibration offset for
 	 * all channels in the signal
 	 */
-	public void setCalibrationOffset(float calibrationOffset) {
-		if (this.calibrationOffset == null || getChannelCount() != this.calibrationOffset.length)
+	public void setCalibrationOffset(float offset) {
+		float[] calibrationOffset = signalParameters.getCalibrationOffset();
+		if (calibrationOffset == null || getChannelCount() != calibrationOffset.length)
 			if (getChannelCount() > 0)
-				this.calibrationOffset = new float[getChannelCount()];
+				calibrationOffset = new float[getChannelCount()];
 			else
-				this.calibrationOffset = new float[1];
-		Arrays.fill(this.calibrationOffset, calibrationOffset);
+				calibrationOffset = new float[1];
+		Arrays.fill(calibrationOffset, offset);
+		signalParameters.setCalibrationGain(calibrationOffset);
 	}
 
 	/**
@@ -336,23 +307,23 @@ public class RawSignalDescriptor {
 	 * @param calibrationOffset new value of calibration offset
 	 */
 	public void setCalibrationOffset(float[] calibrationOffset) {
-		this.calibrationOffset = calibrationOffset;
+		signalParameters.setCalibrationOffset(calibrationOffset);
 	}
 
 	public Float getMinimumValue() {
-		return minimumValue;
+		return signalParameters.getMinimumValue();
 	}
 
 	public void setMinimumValue(Float minimumValue) {
-		this.minimumValue = minimumValue;
+		signalParameters.setMinimumValue(minimumValue);
 	}
 
 	public Float getMaximumValue() {
-		return maximumValue;
+		return signalParameters.getMaximumValue();
 	}
 
 	public void setMaximumValue(Float maximumValue) {
-		this.maximumValue = maximumValue;
+		signalParameters.setMaximumValue(maximumValue);
 	}
 
         /**
@@ -396,7 +367,7 @@ public class RawSignalDescriptor {
          * @return the length of the page in seconds
          */
 	public float getPageSize() {
-		return pageSize;
+		return signalParameters.getPageSize();
 	}
 
         /**
@@ -404,7 +375,7 @@ public class RawSignalDescriptor {
          * @param pageSize the length of the page in seconds
          */
 	public void setPageSize(float pageSize) {
-		this.pageSize = pageSize;
+		signalParameters.setPageSize(pageSize);
 	}
 
         /**
@@ -412,7 +383,7 @@ public class RawSignalDescriptor {
          * @return the number of blocks in one page
          */
 	public int getBlocksPerPage() {
-		return blocksPerPage;
+		return signalParameters.getBlocksPerPage();
 	}
 
         /**
@@ -420,7 +391,7 @@ public class RawSignalDescriptor {
          * @param blocksPerPage the number of blocks in one page
          */
 	public void setBlocksPerPage(int blocksPerPage) {
-		this.blocksPerPage = blocksPerPage;
+		signalParameters.setBlocksPerPage(blocksPerPage);
 	}
 
 	/**

svarog/src/main/java/org/signalml/app/model/document/opensignal/AbstractOpenSignalDescriptor.java
@@ -4,7 +4,7 @@ import org.signalml.domain.montage.system.EegSystem;
 
 public abstract class AbstractOpenSignalDescriptor {
 
-	protected SignalParameters signalParameters;
+	protected SignalParameters signalParameters = new SignalParameters();
 	protected EegSystem eegSystem;
 	
 	public EegSystem getEegSystem() {

svarog/src/main/java/org/signalml/app/model/document/opensignal/ExperimentDescriptor.java
@@ -48,7 +48,6 @@ public class ExperimentDescriptor extends AbstractOpenSignalDescriptor {
 	
 	public ExperimentDescriptor() {
         setBackupFrequency(10.0F);
-        this.signalParameters = new SignalParameters();
 	}
 
 	public String getName() {

svarog/src/main/java/org/signalml/app/model/document/opensignal/SignalParameters.java
@@ -4,9 +4,6 @@ import org.signalml.domain.signal.raw.RawSignalByteOrder;
 import org.signalml.domain.signal.raw.RawSignalSampleType;
 
 public class SignalParameters {
-
-	private RawSignalSampleType sampleType;
-	private RawSignalByteOrder byteOrder;
 	
 	private float samplingFrequency;
 	private int channelCount;
@@ -20,16 +17,11 @@ public class SignalParameters {
 	private Float minimumValue;
 
 	public SignalParameters() {
-		sampleType = RawSignalSampleType.DOUBLE;
-        byteOrder = RawSignalByteOrder.LITTLE_ENDIAN;
         samplingFrequency = 128.0F;
         pageSize = 20.0F;
 	}
 
 	public SignalParameters(SignalParameters signalParameters) {
-		sampleType = signalParameters.sampleType;;
-		byteOrder = signalParameters.byteOrder;
-
 		samplingFrequency = signalParameters.samplingFrequency;
 		channelCount = signalParameters.channelCount;
 		calibrationGain = signalParameters.calibrationGain == null ? null : signalParameters.calibrationGain.clone();
@@ -40,19 +32,6 @@ public class SignalParameters {
 
 		maximumValue = signalParameters.maximumValue;
 		minimumValue = signalParameters.minimumValue;
-	}
-
-	public RawSignalSampleType getSampleType() {
-		return sampleType;
-	}
-	public void setSampleType(RawSignalSampleType sampleType) {
-		this.sampleType = sampleType;
-	}
-	public RawSignalByteOrder getByteOrder() {
-		return byteOrder;
-	}
-	public void setByteOrder(RawSignalByteOrder byteOrder) {
-		this.byteOrder = byteOrder;
 	}
 	public float getSamplingFrequency() {
 		return samplingFrequency;

svarog/src/main/java/org/signalml/app/view/document/opensignal/monitor/SignalParametersPanelForOpenBCI.java
@@ -95,8 +95,6 @@ public class SignalParametersPanelForOpenBCI extends AbstractSignalParametersPan
 						.toString()));
 		int channelCount = getChannelCountSpinner().getValue();
 		signalParameters.setChannelCount(channelCount);
-		signalParameters.setByteOrder((RawSignalByteOrder) getByteOrderComboBox().getSelectedItem());
-		signalParameters.setSampleType((RawSignalSampleType) getSampleTypeComboBox().getSelectedItem());
 		signalParameters.setPageSize(getPageSizeSpinner().getValue());
 
 		List<AmplifierChannel> channels = descriptor.getAmplifier().getSelectedChannels();

svarog/src/main/java/org/signalml/app/worker/monitor/SignalRecorderWorker.java
@@ -15,7 +15,9 @@ import org.apache.log4j.Level;
 
 import org.signalml.app.model.document.opensignal.ExperimentDescriptor;
 import org.signalml.app.model.document.opensignal.SignalParameters;
+import org.signalml.domain.signal.raw.RawSignalByteOrder;
 import org.signalml.domain.signal.raw.RawSignalDescriptor;
+import org.signalml.domain.signal.raw.RawSignalSampleType;
 import org.signalml.domain.signal.raw.RawSignalDescriptor.SourceSignalType;
 import org.signalml.domain.signal.raw.RawSignalDescriptorWriter;
 
@@ -90,6 +92,11 @@ public class SignalRecorderWorker {
          */
         private TagRecorder tagRecorder;
 
+	/**
+	 * Descriptor for recorded samples.
+	 */
+	private RawSignalDescriptor rawSignalDescriptor = new RawSignalDescriptor();
+
         /**
          * Default constructor.
          * @param dataPath path to output file
@@ -121,6 +128,9 @@ public class SignalRecorderWorker {
                 this.firstSampleTimestamp = Double.NaN;
 
                 this.tagRecorder = null;
+
+		rawSignalDescriptor.setSampleType(RawSignalSampleType.DOUBLE);
+		rawSignalDescriptor.setByteOrder(RawSignalByteOrder.LITTLE_ENDIAN);
 
 		logger.setLevel((Level) Level.INFO);
 	}
@@ -190,7 +200,7 @@ public class SignalRecorderWorker {
                 if (chunkCount == 0)
                         return;
                 
-                int sampleSize = monitorDescriptor.getSignalParameters().getSampleType().getByteWidth();
+                int sampleSize = rawSignalDescriptor.getSampleType().getByteWidth();
                 int chunkSize = sampleList.get(0).length * sampleSize;
 
                 byte[] toSave = new byte[chunkSize * chunkCount];
@@ -220,9 +230,9 @@ public class SignalRecorderWorker {
          */
 	private byte[] processChunk(double[] chunk) {
 
-		byte[] byteBuffer = new byte[chunk.length * monitorDescriptor.getSignalParameters().getSampleType().getByteWidth()];
+		byte[] byteBuffer = new byte[chunk.length * rawSignalDescriptor.getSampleType().getByteWidth()];
 
-		ByteBuffer bBuffer = ByteBuffer.wrap(byteBuffer).order(monitorDescriptor.getSignalParameters().getByteOrder().getByteOrder());		
+		ByteBuffer bBuffer = ByteBuffer.wrap(byteBuffer).order(rawSignalDescriptor.getByteOrder().getByteOrder());
 		DoubleBuffer buf = bBuffer.asDoubleBuffer();
 
 		buf.clear();
@@ -247,14 +257,14 @@ public class SignalRecorderWorker {
 		rsd.setExportFileName(dataFilePath);
 		rsd.setBlocksPerPage(1);
 		SignalParameters signalParameters = monitorDescriptor.getSignalParameters();
-		rsd.setByteOrder(signalParameters.getByteOrder());
+		rsd.setByteOrder(rawSignalDescriptor.getByteOrder());
 		rsd.setCalibrationGain(signalParameters.getCalibrationGain());
 		rsd.setCalibrationOffset(signalParameters.getCalibrationOffset());
 		rsd.setChannelCount(signalParameters.getChannelCount());
 		rsd.setChannelLabels(monitorDescriptor.getAmplifier().getSelectedChannelsLabels());
 		rsd.setPageSize(signalParameters.getPageSize());
 		rsd.setSampleCount(savedSampleCount);
-		rsd.setSampleType(signalParameters.getSampleType());
+		rsd.setSampleType(rawSignalDescriptor.getSampleType());
 		rsd.setSamplingFrequency(signalParameters.getSamplingFrequency());
 		rsd.setSourceSignalType(SourceSignalType.RAW);
 		rsd.setFirstSampleTimestamp(firstSampleTimestamp);

commit c2f0577a0f19e87675ebe489fa6d1e4aab749b6f

Calibration offset was not saved while exporting signal.
This bug was introduced in 9fdff9d9d9c0a401085a05079b3fa5fee637fd56
refactorizations - calibration offset was not saved
to the signal descriptor XML file and because of that
the exported signal cannot be opened
(there was a NullPointerException while accessing the
calibration offset values for the signal).

svarog/src/main/java/org/signalml/domain/signal/raw/RawSignalDescriptor.java
@@ -4,15 +4,15 @@
 
 package org.signalml.domain.signal.raw;
 
-import java.util.Date;
-
-import com.thoughtworks.xstream.annotations.XStreamAlias;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.signalml.app.model.document.opensignal.AbstractOpenSignalDescriptor;
 import org.signalml.domain.montage.Montage;
 import org.signalml.domain.montage.system.EegSystem;
 import org.signalml.domain.montage.system.EegSystemName;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
  * This class represents the descriptor of a raw signal.
@@ -295,7 +295,7 @@ public class RawSignalDescriptor extends AbstractOpenSignalDescriptor {
 			else
 				calibrationOffset = new float[1];
 		Arrays.fill(calibrationOffset, offset);
-		signalParameters.setCalibrationGain(calibrationOffset);
+		signalParameters.setCalibrationOffset(calibrationOffset);
 	}
 
 	/**