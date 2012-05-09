524f823c005b22a4c2510cec610c221583f55e4d
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-05-09 18:01:08 +0200

    Extract logging initialization to a separate function

--- svarog/src/main/java/org/signalml/app/SvarogApplication.java
+++ svarog/src/main/java/org/signalml/app/SvarogApplication.java
@@ -344,23 +344,7 @@ public class SvarogApplication implements java.lang.Runnable {
 		if (line.hasOption("moltest"))
 			molTest = true;
 
-		Log4jConfigurer.setWorkingDirSystemProperty("signalml.root");
-
-		// allow for local file config
-		final File loggingConfig = new File(startupDir, "logging.properties");
-		final String loggingPath;
-		if (loggingConfig.exists()) {
-			loggingPath = "file:" + loggingConfig.getAbsolutePath();
-		} else {
-			loggingPath = "classpath:org/signalml/app/logging/log4j_app.properties";
-		}
-
-		try {
-			Log4jConfigurer.initLogging(loggingPath);
-		} catch (FileNotFoundException ex) {
-			System.err.println("Critical error: no logging configuration");
-			System.exit(1);
-		}
+		_init_logging();
 
 		// system properties override file configuration
 		new PropertyConfigurator().configure(System.getProperties());
@@ -451,6 +435,26 @@ public class SvarogApplication implements java.lang.Runnable {
 		});
 
 		logger.debug("SvarogApplication._run complete!");
+	}
+
+	private void _init_logging() {
+		Log4jConfigurer.setWorkingDirSystemProperty("signalml.root");
+
+		// allow for local file config
+		final File loggingConfig = new File(startupDir, "logging.properties");
+		final String loggingPath;
+		if (loggingConfig.exists()) {
+			loggingPath = "file:" + loggingConfig.getAbsolutePath();
+		} else {
+			loggingPath = "classpath:org/signalml/app/logging/log4j_app.properties";
+		}
+
+		try {
+			Log4jConfigurer.initLogging(loggingPath);
+		} catch (FileNotFoundException ex) {
+			System.err.println("Critical error: no logging configuration");
+			System.exit(1);
+		}
 	}
 
 	private void initializeFirstTime(final GeneralConfiguration suggested) {
