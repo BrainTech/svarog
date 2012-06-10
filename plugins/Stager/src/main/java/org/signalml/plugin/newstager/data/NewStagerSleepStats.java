b4648bb16feb46491522b4031f80857d3a7efc92
Konrad Durnoga <kdr@mimuw.edu.pl>
2012-06-10 23:08:42 +0200

    Stager result gets filled properly now

--- plugins/Stager/src/main/java/org/signalml/plugin/newstager/data/NewStagerSleepStats.java
+++ plugins/Stager/src/main/java/org/signalml/plugin/newstager/data/NewStagerSleepStats.java
@@ -2,14 +2,14 @@ package org.signalml.plugin.newstager.data;
 
 public class NewStagerSleepStats {
 
-	public final double alphaThresholds;
+	public final double alphaThreshold;
 	public final double deltaThreshold;
 	public final double spindleThreshold;
 	public final double toneMThreshold;
 
-	public NewStagerSleepStats(double alphaThresholds, double deltaThreshold,
+	public NewStagerSleepStats(double alphaThreshold, double deltaThreshold,
 							   double spindleThreshold, double toneMThreshold) {
-		this.alphaThresholds = alphaThresholds;
+		this.alphaThreshold = alphaThreshold;
 		this.deltaThreshold = deltaThreshold;
 		this.spindleThreshold = spindleThreshold;
 		this.toneMThreshold = toneMThreshold;
