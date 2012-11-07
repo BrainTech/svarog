9486fc2a8ed43581a0ad1ac241b05e5e3f8c446b
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2012-11-07 12:28:27 +0100

    Fix: local scale dialogs sometimes didn't show up after clicking.
    Previously I attempted to fix it in
    652dfc67f96782f8f39814c5b7dd119eb0ea4d84.
    That solution was wrong. The correct one is to
    invoke the revalidate & repaint after removing
    the channelOptionsButtons from the SignalPlotRowHeader.
    (http://stackoverflow.com/a/2236813).

--- svarog/src/main/java/org/signalml/app/view/signal/SignalPlot.java
+++ svarog/src/main/java/org/signalml/app/view/signal/SignalPlot.java
@@ -1196,7 +1196,7 @@ public class SignalPlot extends JComponent implements PropertyChangeListener, Ch
 
 		int currentPage = (int) Math.floor(position.x / pixelPerPage);
 
-		if (masterPlot != null) {
+		if (masterPlot == null) {
 			double timeZoomFactor = ((double) extent.width) / (samplingFrequency*pageSize);
 			setTimeZoomFactor(timeZoomFactor);
 		}
@@ -2392,6 +2392,7 @@ public class SignalPlot extends JComponent implements PropertyChangeListener, Ch
 			double oldValue = this.timeZoomFactor;
 			this.timeZoomFactor = timeZoomFactor;
 			calculateParameters();
+
 			if (horizontalLock) {
 				horizontalPixelLead = (int) Math.round(horizontalTimeLead * pixelPerSecond);
 			}
