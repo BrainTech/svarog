c2ef4a83368b7310cdcbd88eaae260b48669183d
Karol Auguštin <karol@augustin.pl>
2012-12-08 10:14:56 +0100

    Change version to 1.0.2

--- svarog/src/main/java/org/signalml/plugin/loader/PluginLoaderHi.java
+++ svarog/src/main/java/org/signalml/plugin/loader/PluginLoaderHi.java
@@ -278,7 +278,7 @@ public class PluginLoaderHi {
 				URL jarURL = ((JarURLConnection) connection).getJarFileURL();
 				jarFile = _urlToFile(jarURL);
 			} else {
-				// e.g. file:/usr/share/java/svarog-1.0.1-SNAPSHOT.jar
+				// e.g. file:/usr/share/java/svarog-1.0.2-SNAPSHOT.jar
 				jarFile = new File(srcURL.getPath());
 			}
 
