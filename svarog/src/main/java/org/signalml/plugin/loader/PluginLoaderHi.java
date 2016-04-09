1bbf12213936b30899668fc02f22b463543ad02b
Piotr Różański <piotr@develancer.pl>
2016-04-09 02:23:54 +0200

    Bump Svarog version to 1.1.0

--- svarog/src/main/java/org/signalml/plugin/loader/PluginLoaderHi.java
+++ svarog/src/main/java/org/signalml/plugin/loader/PluginLoaderHi.java
@@ -278,7 +278,7 @@ public class PluginLoaderHi {
 				URL jarURL = ((JarURLConnection) connection).getJarFileURL();
 				jarFile = _urlToFile(jarURL);
 			} else {
-				// e.g. file:/usr/share/java/svarog-1.0.10.jar
+				// e.g. file:/usr/share/java/svarog-1.1.0.jar
 				jarFile = new File(srcURL.getPath());
 			}
 
