67f96e5f05ff906e6a2cf5954a4969f6fade9a54
mdovgialo <marian.dowgialo@braintech.pl>
2016-07-20 15:52:10 +0100

    CI builds debs

---
+++ builddeb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mvn clean package
+mkdir dist
+mv svarog-standalone/target/*.deb ./dist
+mv plugins/Artifact/target/*.deb ./dist
+mv plugins/BookReporter/target/*.deb ./dist
+mv plugins/FFTSignalTool/target/*.deb ./dist
+mv plugins/PluginToolCommon/target/*.deb ./dist
+mv plugins/SFTestPlugin/target/*.deb ./dist
+mv plugins/SignalAnalysisPlugin/target/*.deb ./dist
+mv plugins/Stager/target/*.deb ./dist
