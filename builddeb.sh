51d596e71cdb1206da3a239fdae58d127a0758d2
Piotr Różański <piotr.rozanski@braintech.pl>
2016-09-07 11:08:42 +0200

    Names of generated deb files reflect package names
    refs #26451

--- builddeb.sh
+++ builddeb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+function movedeb {
+  mv $1 ./dist/`dpkg -f $1 package`_`dpkg -f $1 version`.deb
+}
 set -e
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$(git describe --tags)
 mvn clean package
 mkdir dist
-mv svarog-standalone/target/*.deb ./dist
-mv plugins/Artifact/target/*.deb ./dist
-mv plugins/BookReporter/target/*.deb ./dist
-mv plugins/FFTSignalTool/target/*.deb ./dist
-mv plugins/PluginToolCommon/target/*.deb ./dist
-#mv plugins/SFTestPlugin/target/*.deb ./dist
-mv plugins/SignalAnalysisPlugin/target/*.deb ./dist
-mv plugins/Stager/target/*.deb ./dist
+movedeb svarog-standalone/target/*.deb
+movedeb plugins/Artifact/target/*.deb
+movedeb plugins/BookReporter/target/*.deb
+movedeb plugins/FFTSignalTool/target/*.deb
+movedeb plugins/PluginToolCommon/target/*.deb
+movedeb plugins/SignalAnalysisPlugin/target/*.deb
+movedeb plugins/Stager/target/*.deb
 sed "s/GIT_VERSION/$(git describe --tags)/g" svarog-all.template > dist/svarog-all
 cd dist
 equivs-build svarog-all
--- signdebs.sh
+++ signdebs.sh
@@ -3,11 +3,11 @@ set -e
 #GPG_KEY - enviroment variable - private key in base64
 echo "$GPG_KEY" | base64 -d | gpg --allow-secret-key-import --import -
 cd dist
-debsigs --sign=origin Artifact_*.deb
-debsigs --sign=origin BookReporter_*.deb
-debsigs --sign=origin FFTSignalTool_*.deb
-debsigs --sign=origin PluginToolCommon_*.deb
-debsigs --sign=origin SignalAnalysisPlugin_*.deb
-debsigs --sign=origin Stager_*.deb
-debsigs --sign=origin svarog-standalone_*.deb
+debsigs --sign=origin svarog-artifact_*.deb
+debsigs --sign=origin svarog-bookreporter_*.deb
+debsigs --sign=origin svarog-fftsignaltool_*.deb
+debsigs --sign=origin svarog-plugintoolcommon_*.deb
+debsigs --sign=origin svarog-signalanalysisplugin_*.deb
+debsigs --sign=origin svarog-stager_*.deb
+debsigs --sign=origin svarog_*.deb
 debsigs --sign=origin svarog-all_*.deb
