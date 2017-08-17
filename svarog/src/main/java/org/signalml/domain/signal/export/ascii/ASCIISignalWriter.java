0cac1d9971594302a689653d6039678bed01886d
Konrad Bednarek <konrad.jan.bednarek@braintech.pl>
2017-08-17 18:43:27 +0200

    r: #35262 Changing ASCIISignalWriter to write samples in chunks in more csv like format.

--- svarog/src/main/java/org/signalml/domain/signal/export/ascii/ASCIISignalWriter.java
+++ svarog/src/main/java/org/signalml/domain/signal/export/ascii/ASCIISignalWriter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.stream.IntStream;
 
 import org.signalml.app.model.signal.SignalExportDescriptor;
 import org.signalml.app.view.signal.SampleSourceUtils;
@@ -47,15 +48,80 @@ public class ASCIISignalWriter implements ISignalWriter {
 		FileWriter fileWriter = new FileWriter(outputFile);
 		this.channelCount = sampleSource.getChannelCount();
 
-		for (int channelNumber = 0; channelNumber < channelCount; channelNumber++) {
-			if(!writeSingleChannel(fileWriter, sampleSource, descriptor, monitor, channelNumber))
-				return;
+		//for (int channelNumber = 0; channelNumber < channelCount; channelNumber++) {
+		//	if(!writeSingleChannel(fileWriter, sampleSource, descriptor, monitor, channelNumber))
+		//		return;
 
-			if (channelNumber < sampleSource.getChannelCount()-1)
-				fileWriter.write("\n");
-		}
+		//	if (channelNumber < sampleSource.getChannelCount()-1)
+		//		fileWriter.write("\n");
+		//}
+                
+		int sampleCount = SampleSourceUtils.getMinSampleCount(sampleSource);
+		int numberOfSamplesToGet = 0;
+                
+                this.writeMultiChannelSamplesHeader(fileWriter, descriptor);
+                for (int sampleNumber = 0; sampleNumber < sampleCount; sampleNumber += numberOfSamplesToGet) {  
+			numberOfSamplesToGet = Math.min(sampleCount - sampleNumber, BUFFER_SIZE);
+                        
+                        double[][] dataChunk = this.getMultiChannelSamplesChunk(sampleSource, sampleNumber, numberOfSamplesToGet);
+                        this.writeMultiChannelSamplesChunk(fileWriter, descriptor, dataChunk, BUFFER_SIZE);
+                        
+                        if (monitor != null) {
+				if (monitor.isRequestingAbort())
+                                    return;
+                                double processedSampleCount = this.channelCount * numberOfSamplesToGet;
+                                processedSampleCount = Math.ceil(processedSampleCount / (this.channelCount * sampleCount));
+                                monitor.setProcessedSampleCount((int) processedSampleCount); 
+			}
+                }
 		fileWriter.close();
 	}
+        
+	/**
+	 * Returns chunk of samples from all channels.
+	 * @param sampleNumber starting point (in time) from which on samples are fetched.
+         * @param numberOfSamplesToGet number of samples which will be fetched for each channel.
+	 * @throws IOException
+	 */
+        private double[][] getMultiChannelSamplesChunk(MultichannelSampleSource sampleSource, int sampleNumber, int numberOfSamplesToGet){
+                double[][] samplesChunk = new double[this.channelCount][];
+                
+                IntStream.range(0, this.channelCount).forEach(
+                        channelNumber -> {
+                            samplesChunk[channelNumber] = new double[numberOfSamplesToGet];
+                            sampleSource.getSamples(channelNumber, samplesChunk[channelNumber], sampleNumber, numberOfSamplesToGet, 0);
+                        }
+                );
+                
+                return samplesChunk;
+        }
+
+	/**
+	 * Exports signal from a multiple channels to file.
+	 * @param samplesChunk double[numberOfChannes][numberOfSamplesInChunk] matrix.
+         * @param chunkSize numberOfSamplesInChunk.
+	 * @throws IOException
+	 */
+        private void writeMultiChannelSamplesChunk(FileWriter output, SignalExportDescriptor descriptor, double[][] samplesChunk, int chunkSize) throws IOException {
+                IntStream.range(0, chunkSize).forEach(
+                        sampleNumber -> {
+                            IntStream.range(0, this.channelCount).forEach(
+                                channelNumber -> {
+                                    output.write(formatter.format(samplesChunk[channelNumber][sampleNumber]) + descriptor.getSeparator());
+                                }
+                            );
+                            output.write("\n");
+                        }
+                );
+        }
+        
+	/**
+	 * Exports channels names as a header to file.
+	 * @throws IOException
+	 */
+        private void writeMultiChannelSamplesHeader(FileWriter output, SignalExportDescriptor descriptor) throws IOException {
+            
+        }
 
 	/**
 	 * Exports signal from a single channel to file.
@@ -71,12 +137,12 @@ public class ASCIISignalWriter implements ISignalWriter {
 		int sampleCount = SampleSourceUtils.getMinSampleCount(sampleSource);
 		int numberOfSamplesToGet = 0;
 
-		for (int sampleNumber = 0; sampleNumber < sampleCount; sampleNumber += numberOfSamplesToGet) {
+		for (int sampleNumber = 0; sampleNumber < sampleCount; sampleNumber += numberOfSamplesToGet) {  
 			numberOfSamplesToGet = Math.min(sampleCount - sampleNumber, BUFFER_SIZE);
 
 			double[] data = new double[numberOfSamplesToGet];
 			sampleSource.getSamples(channelNumber, data, sampleNumber, numberOfSamplesToGet, 0);
-
+                        
 			for (double sample: data) {
 				output.write(formatter.format(sample) + descriptor.getSeparator());
 			}
