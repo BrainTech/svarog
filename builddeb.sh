b831e61b483c625922ff48d920217bea6328ae40
Konrad Bednarek <konrad.jan.bednarek@braintech.pl>
2017-10-13 18:53:44 +0200

    #37319 Adding license

--- builddeb.sh
+++ builddeb.sh
@@ -2,10 +2,24 @@
 function movedeb {
   mv $1 ./dist/`dpkg -f $1 package`_`dpkg -f $1 version`.deb
 }
-set -e
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$(git describe --tags --first-parent)
 
-sed -i s/VRS/$(git describe --tags --first-parent)/g svarog-standalone/src/deb/control/control
+
+VERSION=`git describe --tags --first-parent`
+
+LICENSE_PATH="LICENSE.txt"
+LICENSE_TMP_PATH="/tmp/LICENSE.txt"
+sed 's/VERSION/'"$VERSION"'/' <$LICENSE_PATH >$LICENSE_TMP_PATH
+cp $LICENSE_TMP_PATH $LICENSE_PATH
+
+CONTENTS_PATH="svarog/src/main/resources/org/signalml/help/contents.html"
+CONTENTS_TMP_PATH="/tmp/contets.html"
+sed 's/VERSION/'"$VERSION"'/' <$CONTENTS_PATH >$CONTENTS_TMP_PATH
+cp CONTENTS_TMP_PATH CONTENTS_PATH
+
+set -e
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$(VERSION)
+
+sed -i 's/VRS/'"$VERSION"'/g' svarog-standalone/src/deb/control/control
 
 # Skip tests without even compiling them. Whe are compiling them and running in previous job, so it isn't necessary.
 # To compile tests run `mvn test-compile compile`
