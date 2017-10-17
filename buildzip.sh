266b23bfecc01fa38551268cf75c9538e1b81921
Konrad Bednarek <konrad.jan.bednarek@braintech.pl>
2017-10-17 16:26:00 +0200

    #37618 Correct text in contents license. Fixing typos in bash script.

--- buildzip.sh
+++ buildzip.sh
@@ -1,39 +1,16 @@
 #!/bin/bash
+source ./functions.sh
 set -e
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$(git describe --tags --first-parent)
-# Skip tests without even compiling them. Whe are compiling them and running in previous job, so it isn't necessary.
-# To compile tests run `mvn test-compile compile`
-mvn clean package -DskipTests
 
-VERSION=$(git describe --tags --first-parent)
+build
 
-mkdir -p dist/svarog-$VERSION-standalone/plugins
+dest="dist/svarog-$VERSION-standalone"
+mkdir -p "$dest/plugins"
 
-
-mv standalone-package-files/* dist/svarog-$VERSION-standalone/
-
-
-mv svarog-standalone/target/*.jar dist/svarog-$VERSION-standalone/svarog-standalone.jar
-
-mv plugins/Artifact/target/*.jar dist/svarog-$VERSION-standalone/plugins/
-mv plugins/Artifact/target/*.xml dist/svarog-$VERSION-standalone/plugins/
-
-mv plugins/BookReporter/target/*.jar dist/svarog-$VERSION-standalone/plugins/
-mv plugins/BookReporter/target/*.xml dist/svarog-$VERSION-standalone/plugins/
-
-mv plugins/FFTSignalTool/target/*.jar dist/svarog-$VERSION-standalone/plugins/
-mv plugins/FFTSignalTool/target/*.xml dist/svarog-$VERSION-standalone/plugins/
-
-mv plugins/PluginToolCommon/target/*.jar dist/svarog-$VERSION-standalone/plugins/
-mv plugins/PluginToolCommon/target/*.xml dist/svarog-$VERSION-standalone/plugins/
-
-mv plugins/SignalAnalysisPlugin/target/*.jar dist/svarog-$VERSION-standalone/plugins/
-mv plugins/SignalAnalysisPlugin/target/*.xml dist/svarog-$VERSION-standalone/plugins/
-
-mv plugins/Stager/target/*.jar dist/svarog-$VERSION-standalone/plugins/
-mv plugins/Stager/target/*.xml dist/svarog-$VERSION-standalone/plugins/
-
-cp LICENSE.txt dist/svarog-$VERSION-standalone/
+mv standalone-package-files/* "$dest"
+mv svarog-standalone/target/*.jar "$dest/svarog-standalone.jar"
+mv plugins/*/target/*.{jar,xml} "$dest/plugins/"
+cp LICENSE.txt "$dest"
 
 cd dist
 zip -r svarog-$VERSION-standalone.zip svarog-$VERSION-standalone
