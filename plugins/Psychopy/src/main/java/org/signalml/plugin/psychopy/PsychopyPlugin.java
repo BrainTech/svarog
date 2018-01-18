b83fee666b42383c680a79398080f4c1685d5539
Konrad Bednarek <konrad.jan.bednarek@braintech.pl>
2018-01-18 14:15:32 +0100

    #39332 Initial commit for Psychopy plugin

---
+++ plugins/Psychopy/src/main/java/org/signalml/plugin/psychopy/PsychopyPlugin.java
@@ -0,0 +1,11 @@
+package org.signalml.plugin.psychopy;
+
+import org.signalml.plugin.export.Plugin;
+import org.signalml.plugin.export.SvarogAccess;
+
+public class PsychopyPlugin implements Plugin {
+	@Override
+	public void register(SvarogAccess access) throws Exception {
+
+	}
+}
